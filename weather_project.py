a0ecbf3fa18a53c8a4910d0fad16c15adb84f868
Ajith-Ak-code <71313182+Ajith-Ak-code@users.noreply.github.com>
2023-07-27 18:18:58 +0530

    Final Output of weather report

--- weather_project.py
+++ weather_project.py
@@ -1,35 +1,44 @@
 import requests
 import json
+import sys
 
 data=requests.get('https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22')
-date_component=[]
 
-date_input,time_input=data.json()['list'][0]['dt_txt'].split(' ')
+date_component=''
+date_components = input('Enter a date formatted as YYYY-MM-DD: ')
+date_component=date_component+date_components
 
-def temp():
-	temps=data.json()['list'][0]['main']['temp']
-	print("Temparature:",temps)
-def wind():
-	winds=data.json()['list'][0]['wind']['speed']
-	print("wind speed: ",winds)
-def pressure():
-	pressures=data.json()['list'][0]['main']['pressure']
-	print("Pressure: ",pressures)
+date_input=[]
+temp=[]
+wind=[]
+press=[]
 
+for i in range(sys.maxsize):
+	dat,tym=data.json()['list'][i]['dt_txt'].split(' ')
+	if dat==date_component:
+		temps=data.json()['list'][i]['main']['temp']
+		temp.append(temps)
+		winds=data.json()['list'][i]['wind']['speed']
+		wind.append(winds)
+		pressure=data.json()['list'][i]['main']['pressure']
+		press.append(pressure)
+		break
+	elif dat not in date_input:
+		date_input.append(dat)
+
+	
 while True:
-	print("Press 1 , Get Temparature")
+	print("Press 1 , Get Temp")
 	print("Press 2 , Get Wind Speed")
 	print("Press 3 , Get Pressure")
 	print("Press 0 , Exit")
 	choice=int(input('Enter your choice: '))
-	date_components = input('Enter a date formatted as YYYY-MM-DD: ')
-	date_component.append(date_components)
 	if choice==1:
-		temp()
+		print('Temp: ',temp)
 	elif choice==2:
-		wind()
+		print('wind: ',wind)
 	elif choice==3:
-		pressure()
+		print('pressure: ',press)
 	elif choice==0:
 		print('Thank you')
 		quit()
